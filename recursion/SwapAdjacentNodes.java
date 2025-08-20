a49c3fbf67607d844412f9194e1d381eb7d8cde9
Prashant Sharma <ps.arraha@gmail.com>
2025-08-20 17:38:34 +0200

    swap-nodes-in-pairs

--- recursion/SwapAdjacentNodes.java
+++ recursion/SwapAdjacentNodes.java
@@ -6,13 +6,13 @@ class SwapAdjacentNodes {
             return head;
         }
 
-        ListNode n1 = head;
-        ListNode n2 = head.next;
+        ListNode n1 = head; // First node of current pair
+        ListNode n2 = head.next; // Second node of current pair
 
-        n1.next = swapPairs(n2.next);
-        n2.next = n1;
+        n1.next = swapPairs(n2.next); // Recursively process remaining pairs
+        n2.next = n1; // Connect second node to first
 
-        return n2;
+        return n2; // Return new head of this pair
     }
  
 }
